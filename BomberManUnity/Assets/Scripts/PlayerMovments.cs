34349709f3ad4d3b23a1a72382c1463c35fa7a4b
Paul <pl.sainrat@outlook.com>
2019-04-10 10:57:18 +0200

    Script playerMovment

---
+++ BomberManUnity/Assets/Scripts/PlayerMovments.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovments : MonoBehaviour
+{
+
+    private float horizontalMvmnt;
+    private float verticalMvmnt;
+    private float speed;
+
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+        speed = 20f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMvmnt = Input.GetAxis("Horizontal");
+        verticalMvmnt = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontalMvmnt * speed, verticalMvmnt * speed);
+    }
+}
