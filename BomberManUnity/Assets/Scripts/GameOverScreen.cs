3029c3db501f1c8d606d05d7bbacee879085f9d7
Paul <pl.sainrat@outlook.com>
2019-04-17 00:30:10 +0200

    Death Screen Et reset de la scene

---
+++ BomberManUnity/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Invoke("ResetScene", 5f);
+    }
+
+    private void ResetScene()
+    {
+        SceneManager.LoadScene("CombatScene");
+    }
+}
---
+++ BomberManUnity/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameManager;
+
+    private void OnDestroy()
+    {
+        gameManager.active = true; 
+    }
+}
